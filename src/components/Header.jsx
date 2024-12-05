63e4a56b26bfe6390b20ca18c02668da5ca8e90a
Daniel B Sherry <dbsherry890@gmail.com>
2024-12-05 15:40:30 -0600

    burger icon fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -58,7 +58,7 @@ function Header() {
           <div className="md:hidden flex items-center lg:order-1">
             <button
               type="button"
-              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 ml-1 text-sm text-gray-100 rounded-lg hover:bg-gray-600 "
               aria-controls="mobile-menu"
               aria-expanded={nav}
               onClick={() => setNav(!nav)}
